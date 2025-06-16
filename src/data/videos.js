8b9827b05c5e173a62d64d6ea829811996ecd66a
AnmolMandhan <151067526+AnmolMandhan@users.noreply.github.com>
2025-06-16 20:58:42 +0500

    videos.js

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,1 +1,32 @@
+const videos = [
+  {
+    id: 1,
+    title: 'Learn React in 5 Minutes',
+    thumbnail: 'https://img.youtube.com/vi/Ke90Tje7VS0/hqdefault.jpg',
+    embedUrl: 'https://www.youtube.com/embed/Ke90Tje7VS0',
+    category: 'Tech',
+  },
+  {
+    id: 2,
+    title: 'Top 10 Gaming Moments',
+    thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/hqdefault.jpg',
+    embedUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+    category: 'Gaming',
+  },
+  {
+    id: 3,
+    title: 'Relaxing Piano Music',
+    thumbnail: 'https://img.youtube.com/vi/1ZYbU82GVz4/hqdefault.jpg',
+    embedUrl: 'https://www.youtube.com/embed/1ZYbU82GVz4',
+    category: 'Music',
+  },
+  {
+    id: 4,
+    title: 'Latest Tech News June 2025',
+    thumbnail: 'https://img.youtube.com/vi/qhg4WEZ5T8g/hqdefault.jpg',
+    embedUrl: 'https://www.youtube.com/embed/qhg4WEZ5T8g',
+    category: 'News',
+  },
+];
+export default videos;
 
